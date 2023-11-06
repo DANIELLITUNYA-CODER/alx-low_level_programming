ecdb6e56483bfa3feb360ca58768b634d40cb357
DANIELLITUNYA-CODER <litunyadaniel@gmail.com>
2023-11-05 22:49:40 -0800

    preprocessor

---
+++ 0x0D-preprocessor/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+#define SIZE 1024
+#define PI 3.14159265359
+
+#define ABS(x) ((x) < 0 ? -(x) : (x))
+#define SUM(x, y) ((x) + (y))
+
+#endif /* MAIN_H */
+
